237ca693426eab89adcfd539e01b17a7aa09b62e
Emiliano Firmino <emiliano.firmino@gmail.com>
2015-03-25 20:41:18 -0300

    Código resposta para Q1.1 e Q1.2
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

---
+++ 03/dakerfp_ecdmf_dining_philosopher_problem/dpp1.c
@@ -0,0 +1,73 @@
+/*
+ * dining_philosopher_01.c
+ * Copyright (C) 2015 Emiliano Firmino <emiliano.firmino@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "thread.h"
+
+#define N 5
+
+typedef struct context {
+    int id;
+    MUTEX_T* left_chopstick;
+    MUTEX_T* right_chopstick;
+} context_t;
+
+THREAD_FUNC(print_hello, param) {
+    context_t* ctx = param;
+
+    for (;;) {
+        SLEEP(rand() % 5);
+        printf("%d is hungry!\n", ctx->id);
+        MUTEX_LOCK(*ctx->right_chopstick);
+        printf("%d got right chopstick\n", ctx->id);
+        MUTEX_LOCK(*ctx->left_chopstick);
+        printf("%d got left chopstick\n", ctx->id);
+
+        printf("%d start eating\n", ctx->id);
+        SLEEP(1);
+        printf("%d finish eating\n", ctx->id);
+
+        MUTEX_UNLOCK(*ctx->right_chopstick);
+        printf("%d release right chopstick\n", ctx->id);
+        MUTEX_UNLOCK(*ctx->left_chopstick);
+        printf("%d release left chopstick\n", ctx->id);
+    }
+
+    free(ctx);
+    return 0;
+}
+
+int main() {
+    srand(time(NULL));
+    MUTEX_T  chopsticks[N];
+    THREAD_T philosophers[N];
+
+    for (int i = 0; i < N; i++) {
+        MUTEX_INIT(chopsticks[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        context_t* ctx = (context_t*) malloc(sizeof(context_t));
+        ctx->id = i;
+        ctx->left_chopstick = &chopsticks[i];
+        ctx->right_chopstick = &chopsticks[(i+1) % N];
+
+        THREAD_INIT(philosophers[i], print_hello, ctx);
+    }
+
+    for (int i = 0; i < N; i++) {
+        THREAD_JOIN(philosophers[i]);
+        THREAD_DESTROY(philosophers[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        MUTEX_DESTROY(chopsticks[i]);
+    }
+
+    return 0;
+}
---
+++ 03/dakerfp_ecdmf_dining_philosopher_problem/thread.h
@@ -0,0 +1,64 @@
+/*
+ * thread.h
+ * Copyright (C) 2015 Daker Fernandes, Emiliano Firmino
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef __THREAD_H__
+#define __THREAD_H__
+
+#if defined(__unix__) || defined(__APPLE__)
+    #include <semaphore.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <unistd.h>
+    #include <pthread.h>
+
+    #define THREAD_T             pthread_t
+    #define THREAD_FUNC(N, P)    void* N (void* P)
+    #define THREAD_INIT(T, F, P) pthread_create(&T, NULL, F, P)
+    #define THREAD_JOIN(T)       pthread_join(T, NULL)
+    #define THREAD_DESTROY(T)
+
+    #define MUTEX_T          pthread_mutex_t
+    #define MUTEX_INIT(M)    pthread_mutex_init(&M, NULL)
+    #define MUTEX_LOCK(M)    pthread_mutex_lock(&M)
+    #define MUTEX_TRYLOCK(M) pthread_mutex_trylock(&M) == 0
+    #define MUTEX_UNLOCK(M)  pthread_mutex_unlock(&M)
+    #define MUTEX_DESTROY(M) pthread_mutex_destroy(&M)
+
+    #define SEM_T          sem_t
+    #define SEM_INIT(S)    sem_init(&S, 0, 0)
+    #define SEM_UP(S)      sem_post(&S)
+    #define SEM_DOWN(S)    sem_wait(&S)
+    #define SEM_DESTROY(S) sem_close(&S)
+
+    #define SLEEP(T)       sleep(T)
+#elif _WIN32
+    #include <limits.h>
+    #include <windows.h>
+
+    #define THREAD_T             HANDLE
+    #define THREAD_FUNC(N, P)    DWORD WINAPI N (LPVOID P)
+    #define THREAD_INIT(T, F, P) T = CreateThread(NULL, 0, F, P, 0, NULL)
+    #define THREAD_JOIN(T)       WaitForSingleObject(T, INFINITE)
+    #define THREAD_DESTROY(T)    CloseHandle(T)
+
+    #define MUTEX_T          HANDLE
+    #define MUTEX_INIT(M)    M = CreateMutex(NULL, FALSE, NULL)
+    #define MUTEX_LOCK(M)    WaitForSingleObject(M, INFINITE)
+    #define MUTEX_UNLOCK(M)  ReleaseMutex(M)
+    #define MUTEX_DESTROY(M) CloseHandle(M)
+
+    #define SEM_T          HANDLE
+    #define SEM_INIT(S)    S = CreateSemaphore(NULL, 0, INT_MAX, NULL)
+    #define SEM_UP(S)      ReleaseSemaphore(S, 1, NULL)
+    #define SEM_DOWN(S)    WaitForSingleObject(S, INFINITE)
+    #define SEM_DESTROY(S) CloseHandle(S)
+
+    #define SLEEP(T)       Sleep(T)
+#endif
+
+#endif /* !__THREAD_H__ */
+
